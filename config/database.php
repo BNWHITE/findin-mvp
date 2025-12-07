dd4094f5746cafe079bcf3394aaae0d2b94d9f5c
BNWHITE <blacknwhitemanagement@gmail.com>
2025-12-07 17:34:51 +0100

    Create database.php

---
+++ config/database.php
@@ -0,0 +1,14 @@
+<?php
+// config/database.php
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'gestion_competences');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_CHARSET', 'utf8mb4');
+
+// Rôles utilisateurs
+define('ROLE_EMPLOYEE', 'employe');
+define('ROLE_MANAGER', 'manager');
+define('ROLE_RH', 'rh');
+define('ROLE_ADMIN', 'admin');
+?>
